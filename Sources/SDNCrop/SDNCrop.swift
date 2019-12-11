11120632182f64b5bf8ad606cf469eb2e5a477c9
John Grange <john@sd-networks.net>
2019-12-10 20:51:22 -0800

    Initial commit

--- Sources/SDNCrop/SDNCrop.swift
+++ Sources/SDNCrop/SDNCrop.swift
@@ -1,3 +1,76 @@
-struct SDNCrop {
-    var text = "Hello, World!"
+
+import UIKit
+
+private(set) var bundle: Bundle? = {
+    return SDNCrop.Config.bundle
+} ()
+
+public func cropViewController(image: UIImage, config: Config = Config()) -> CropViewController {
+    return CropViewController(image: image, config: config, mode: .normal)
+}
+
+public func cropCustomizableViewController(image: UIImage, config: Config = Config()) -> CropViewController {
+    return CropViewController(image: image, config: config, mode: .customizable)
+}
+
+public typealias CropInfo = (translation: CGPoint, rotation: CGFloat, scale: CGFloat, cropSize: CGSize, imageViewSize: CGSize)
+
+public func getCroppedImage(byCropInfo info: CropInfo, andImage image: UIImage) -> UIImage? {
+    return image.getCroppedImage(byCropInfo: info)
+}
+
+// Config
+public struct Config {
+
+    public var ratioOptions: RatioOptions = .all
+    var customRatios: [(width: Int, height: Int)] = []
+
+    public var presetFixedRatioType: PresetFixedRatioType = .canUseMultiplePresetFixedRatio
+
+    public var showRotationDial = true
+
+    public init() {
+    }
+
+    static var bundleIdentifier: String?
+
+    static private(set) var bundle: Bundle? = {
+
+        guard let bundleIdentifier = Config.bundleIdentifier else { return nil }
+
+        guard let bundle = Bundle(identifier: bundleIdentifier) else {
+            return nil
+        }
+
+        if let url = bundle.url(forResource: "Resource", withExtension: "bundle") {
+            let bundle = Bundle(url: url)
+            return bundle
+        }
+        return nil
+    } ()
+
+    mutating public func addCustomRatio(byHorizontalWidth width: Int, andHorizontalHeight height: Int) {
+        customRatios.append((width, height))
+    }
+
+    mutating public func addCustomRatio(byVerticalWidth width: Int, andVerticalHeight height: Int) {
+        customRatios.append((height, width))
+    }
+
+    func hasCustomRatios() -> Bool {
+        return customRatios.count > 0
+    }
+
+    func getCustomRatioItems() -> [RatioItemType] {
+        return customRatios.map {
+            (String("\($0.width):\($0.height)"), Double($0.width)/Double($0.height), String("\($0.height):\($0.width)"), Double($0.height)/Double($0.width))
+        }
+    }
+}
+
+public enum PresetFixedRatioType {
+    /** When choose alwaysUsingOnePresetFixedRatio, fixed-ratio setting button does not show.
+     */
+    case alwaysUsingOnePresetFixedRatio(ratio: Double)
+    case canUseMultiplePresetFixedRatio
 }
--- Tests/SDNCropTests/SDNCropTests.swift
+++ Tests/SDNCropTests/SDNCropTests.swift
@@ -6,7 +6,8 @@ final class SDNCropTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SDNCrop().text, "Hello, World!")
+
+        
     }
 
     static var allTests = [
